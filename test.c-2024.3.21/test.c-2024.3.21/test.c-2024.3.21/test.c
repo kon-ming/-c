6e674a70f73d441c163dc60e3f66204d82e7dbf4
kong_-_ming <3042744622@qq.com>
2024-03-21 19:35:59 +0800

    猜数字游戏

---
+++ test.c-2024.3.21/test.c-2024.3.21/test.c-2024.3.21/test.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+//void menu()
+//{
+//	printf("*******************\n");
+//	printf("*******1.play******\n");
+//	printf("*******0.exit******\n");
+//	printf("*******************\n");
+//}
+//void game()
+//{
+//	int guess = 0;
+//	int ret = rand()%100+1;//生成一个随机数的函数
+//	//printf("%d\n", ret);
+//	while(1)
+//	{
+//		printf("请猜数字:>");
+//	    scanf("%d", &guess);
+//	    if (guess < ret) 
+//	     {
+//         printf("猜小了\n");
+//	     }		
+//	     else if (guess > ret) 
+//	      {
+//          printf("猜大了\n");
+//	      }   		
+//	      else
+//	      {
+//		   printf("恭喜猜对了\n");
+//		   break;
+//	       }
+//    }
+//}
+//int main()//猜数字游戏
+//{
+//	int input = 0;
+//    srand((unsigned int)time(NULL));
+//do
+//	{
+//		menu();
+//		printf("请选择:>");
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case 1:
+//			game();
+//			break;
+//		case 0:
+//			printf("退出游戏\n");
+//			break;
+//		default:
+//			printf("选择错误，重新选择\n");
+//			break;
+//
+//		}
+//	} while (input);
+//
+//	return 0;
+//}
